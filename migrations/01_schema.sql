ae393d45ffbd403b627534efa125ded2edcd14c6
Jodie Drover <jodiedrover@live.com>
2022-10-26 17:59:28 -0400

    Rearrange order of table creations

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,3 +1,10 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255),
+);
+
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
@@ -15,13 +22,6 @@ CREATE TABLE properties (
   province VARCHAR(255),
   post_code VARCHAR(255),
   active BOOLEAN
-);
-
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
-  email VARCHAR(255),
-  password VARCHAR(255),
 );
 
 CREATE TABLE reservations (
