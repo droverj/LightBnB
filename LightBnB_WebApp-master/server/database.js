cb4dd47e159da3abebfe4f6642da8681aae9150a
Jodie Drover <jodiedrover@live.com>
2022-11-02 19:58:17 -0400

    Update getUserWithEmail function to use query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,17 +17,16 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+const getUserWithEmail = email => {
+  return pool
+  .query(`SELECT * FROM users WHERE email = $1`, [email])
+  .then((result) => {
+    console.log(result.rows[0]);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getUserWithEmail = getUserWithEmail;
 
